9c607ac64dd3ae21c4e5dd5317843b209aa3214c
hilmiubuntu <rusydiansyahhilmi@gmail.com>
2022-08-26 21:13:53 +0700

    coba cek lagi step-step yang kubuat sama di video tutorialnya

---
+++ simple_server_rospy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import rospy
+from (srvPackageName).srv import CountWords, CountWordsResponse
+
+def server_callback(req):
+    rospy.loginfo("Received request: %s", req.words.data)
+
+    words = req.words.data.split(" ")
+    resp = CountWordsResponse()
+
+    if req.count_articles.data:
+        english_articles = ["a", "an", "the"]
+        resp.num_articles.data = len([word for word in words
+                                      if word in english_articles])
+    
+    resp.num_words.data = len(words) - resp.num_articles.data
+
+    return resp
+
+if __name__ == '__main__':
+    rospy.init_node('rospy_simple_server_node', anonymous=False)
+    rospy.Service('rospy_word_count', CountWords, server_callback)
+    rospy.spin()
